cc093dc389eaf6692585cd42ffffd0027541dc33
Brenda Rayane <brenda.rayane.0018@gmail.com>
2022-09-25 17:24:36 -0300

    Palavra sorteada sendo preenchida na tela conforme acertos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import palavras from "./palavras"
 import forca0 from "./image/forca0.png"
+import forca1 from "./image/forca1.png"
+import forca2 from "./image/forca2.png"
+import forca3 from "./image/forca3.png"
+import forca4 from "./image/forca4.png"
+import forca5 from "./image/forca5.png"
+import forca6 from "./image/forca6.png"
 import { useState } from "react"
 
 export default function App() {
+    const [imagem, setImagem]=useState(forca0)
+    const [contador, setContador]=useState(0)
     const [desabilitado, setDesabilitado] = useState(true)
     const [palavraCoparacao, setPalavraComparacao] = useState("") //palavra formato string
     const [palavraSorteada, setPalavraSorteada] = useState([]) //palavra formato array
     const [palavraRenderizada, setPalavraRenderizada] = useState([]) //palavra escondida "_"
     const [input, setInput] = useState("")
+    let arrayImagens= [forca0,forca1, forca3, forca4, forca5, forca6]
+    let palavraSendoPreenchida= [...palavraRenderizada]
 
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
         "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -25,8 +35,24 @@ export default function App() {
     }
 
     function compararLetra(letra) {
-        alert("clicou")
-        console.log(letra)
+        if(palavraSorteada.includes(letra)){
+            for(let i=0; i<palavraSorteada.length; i++){
+                if(palavraSorteada[i]===letra){
+                    palavraSendoPreenchida[i]=letra 
+                    console.log(palavraSendoPreenchida)
+                    console.log(palavraRenderizada)      
+                }
+            }
+            //palavraSendoPreenchida = palavraSorteada.map((l)=>(letra===l ? letra:" _ "))
+            setPalavraRenderizada(palavraSendoPreenchida)
+        } else{
+            let contagemErro= contador+1
+            setContador(contagemErro)
+            console.log(contador)
+            console.log(contagemErro)
+            let imagemATual= arrayImagens[contagemErro]
+            setImagem(imagemATual)
+        }
     }
 
     function chutarPlavra() {
@@ -34,6 +60,7 @@ export default function App() {
             console.log("igual")
         } else {
             console.log("diferente")
+            setImagem(forca6)
         }
         setInput("")
 
@@ -44,7 +71,7 @@ export default function App() {
         <div className="container">
             <div className="imagem-e-botao">
                 <div className="imagem">
-                    <img src={forca0} />
+                    <img src={imagem} />
                 </div>
                 <div className="botao-escolher">
                     <div>
